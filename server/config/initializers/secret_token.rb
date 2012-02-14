c064b3de419c63dd2e5da6c7c5920aaecb02f11f
Mateusz Bilski <mateusz.bilski@gmail.com>
2012-02-14 18:41:33 +0100

    init

---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dziura::Application.config.secret_token = '1227692ee97b9d9e184ef11d207488b09941f86bac52a53d8c5bf627f4a01b12f96f8d5739e176e49c51a111d3c11ee3d741b78645362e260be621c8ace71b60'
