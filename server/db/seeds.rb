88ed7394a9a6ae2ccf390214aa5141048cb01731
Piotr Dziwinski <piotrdz@gmail.com>
2012-03-10 20:18:41 +0100

    Jeszcze przykładowe dane
    Zapomniałem napisać, że w poprzednim commicie jeszcze dodałem przykładowe dane w db/seeds.rb

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
 
+
 # Na razie bez adresu
 unit = Unit.create(:name => "ZDIUM")
 
