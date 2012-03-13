143389def0accf34ed30283c177362edf67df487
derwu <derwu22@gmail.com>
2012-03-13 02:59:54 -0700

    Kolejna wersja robocza metody add_issue

--- server/app/models/issue.rb
+++ server/app/models/issue.rb
@@ -17,15 +17,35 @@ class Issue < ActiveRecord::Base
     l.save
   end
 
-#  def add_issue (photo, desc, notificar_email, category_id, longitude, latitude)
-#    i = issue.where(:category_id => category_id, :latitude => latitude, :longitude => longitude)
-#
-#    if !i.exist()
-#      issue_new = issue.new :latitude => latitude, :longitude => longitude, :desc => desc, :category_id => category_id
-#      issue_new.save
-#    end
-#
-#    issue_instance_new = issue_instance.new :latitude => latitude, :longitude => longitude, :desc => desc, :notificar_email => notificar_email
-#    issue_instance_new.save
-#  end
+  # Metoda sluzaca do dodawania nowego issue oraz issue_instance
+  #
+  # Metoda probuje dopasowac issue, jezeli to sie nie uda tworzy nowy issue.
+  # Metoda zawsze tworzy nowy issue_instance i przypisuje go do odpowiedniego issue (dopasowanego lub nowego).
+  ## czy ta metoda nie powinna byc raczej w issue_instance (przyjmuje argumenty z REST issue_instance)
+  ## brakuje obslugi zdjec
+  def add_issue (photo, desc, notificar_email, category_id, longitude, latitude)
+    # dokladnosc dopasowania issue
+    precision_latitude = 0.0001
+    precision_longitude = 0.0001
+
+    # szukanie istniejacego issue
+    i = issue.where(:category_id => category_id, :latitude => (latitude-precision_latitude)..(latitude+precision_latitude), :longitude => (longitude-precision_longitude)..(longitude+precision_longitude))
+
+    # jezeli nie dopasowano zadnego issue
+    ## czy ponizszy zapis jest prawidlowy?
+    if !i.exist()
+      ## czy trzeba recznie sprawdzac i podawac do jakiego unit nalezy dany issue?
+      ## czy ustawiac status_id, czy ustawi sie jakis domyslny?
+      ## chyba warto sprawdzac poprawnosc category_id?
+      i = issue.new :latitude => latitude, :longitude => longitude, :category_id => category_id
+      i.save
+    end
+
+    # tworzenie nowego issue_instance
+    ## jak przyporzadkowac issue_instance do issue?
+    ## nalezy tworzyc obiekt address i uzupelnic go na podstawie danych z zewnetrznego API
+    ## nie mam pojecia jak wykorzystac google api w railsach
+    issue_instance_new = issue_instance.new :latitude => latitude, :longitude => longitude, :desc => desc, :notificar_email => notificar_email
+    issue_instance_new.save
+  end
 end
