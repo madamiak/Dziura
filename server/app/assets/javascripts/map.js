abb5af52ae9020eddfdfbb4856dd80169393769c
Mateusz Adamiak <mateusz@Ander.(none)>
2012-03-15 18:44:36 +0100

    fix

--- server/app/assets/javascripts/map.js
+++ server/app/assets/javascripts/map.js
@@ -5,25 +5,39 @@ var infowindow;
 
 function initialize() {
 	var myLatlng = new google.maps.LatLng(51.110,17.030);
-	
+
 	var myOptions = {
 		zoom: 14,
 		center: myLatlng,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	}
-	
+
 	existing = false;
-	
+
 	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-	
+
 	google.maps.event.addListener(map, 'click', function(event, isExisted) {
 		placeMarker(event.latLng, existing);
 	});
-	
 }
 
 function setExisting(value) {
 	existing = value;
+}
+
+function bindIssueForm() {
+	$("#issue_form form").bind("submit", function() {
+		$("#issue_form input[name=longitude]").val(marker.getPosition().lng());
+		$("#issue_form input[name=latitude]").val(marker.getPosition().lat());
+	});
+
+	$("#issue_form form").live("ajax:success", function(event, data, status, xhr) {
+		$("#issue_message").html(data.responseText);
+	});
+
+	$("#issue_form form").live("ajax:error", function(event, data, status, xhr) {
+		$("#issue_message").html(data.responseText);
+	});
 }
 
 function placeMarker(location, isExisted) {
@@ -33,30 +47,27 @@ function placeMarker(location, isExisted) {
 			map: map,
 			draggable: true
 		});
-		
-		//pobieranie formularza zgloszenia szkody z serwera	
-	  	$.get('res/issue', function(data) {
-      		infowindow = new google.maps.InfoWindow({
-			content: data
-		});
-		
-		infowindow.open(map, marker);	
-		  
-		google.maps.event.addListener(infowindow, 'domready', function() {
-			$("#issue_form form").submit(function() {
-				$("#issue_form input[name=longitude]").val(marker.getPosition().lng());
-				$("#issue_form input[name=latitude]").val(marker.getPosition().lat());
-			});
-		});
-    });	
 
-	google.maps.event.addListener(marker, 'click', function() {
+		//pobieranie formularza zgloszenia szkody z serwera
+		$.get('res/issue', function(data) {
+			infowindow = new google.maps.InfoWindow({
+				content: data
+		});
+
+		infowindow.open(map, marker);
+
+		google.maps.event.addListener(infowindow, 'domready', function() {
+			bindIssueForm();
+		});
+    });
+    
+    google.maps.event.addListener(marker, 'click', function() {
 		infowindow.open(map, marker);
 	});
 
 	setExisting(true);
 	} 
 	else {
-		marker.setPosition(location);			
+		marker.setPosition(location);
 	}
 }
