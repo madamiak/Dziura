0b6d0e2b03226a7ef72668826bbb24ec4402cd13
Piotr Dziwinski <piotrdz@gmail.com>
2012-03-28 22:13:20 +0200

    Wykorzystanie debuggera
    Na pewno się przyda :)

--- server/config/environment.rb
+++ server/config/environment.rb
@@ -4,3 +4,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Dziura::Application.initialize!
+
+# Do debuggera
+SCRIPT_LINES__ = {} if ENV['RAILS_ENV'] == 'development'
