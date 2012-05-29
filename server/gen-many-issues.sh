c2774610644e9874b609a88e568c21034d3c90de
Mateusz Bilski <mateusz.bilski@gmail.com>
2012-05-29 12:05:01 +0200

    zeby testy dzialaly

--- server/gen-many-issues.sh
+++ server/gen-many-issues.sh
@@ -4,18 +4,18 @@
 
 rails console <<< '
 # 400 takich samych instancji
-for i in 1..400 do
+for i in 1..10 do
   Issue.add_issue(1, "17.04126", "51.11744", "foo #{i}", nil, nil)
 end
 
 # 400 zgłoszeń losowo w prostokącie
-latMin = BigDecimal.new("51.11839")
-latMax = BigDecimal.new("51.12319")
-lngMin = BigDecimal.new("17.02911")
-lngMax = BigDecimal.new("17.03908")
-for i in 1..400 do
+latMin = BigDecimal.new("51.096785")
+latMax = BigDecimal.new("51.124159")
+lngMin = BigDecimal.new("17.012844")
+lngMax = BigDecimal.new("17.063956")
+for i in 1..50 do
   rnd_lat = (latMin + (latMax - latMin) * rand).to_s
   rnd_lng = (lngMin + (lngMax - lngMin) * rand).to_s
-  Issue.add_issue(2, rnd_lng, rnd_lat, "bar #{i}", nil, nil)
+  Issue.add_issue(rand(5)+1, rnd_lng, rnd_lat, "bar #{i}", nil, nil)
 end
 quit'
